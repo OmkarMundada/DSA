20529b922ccb8f878f70231439c2979b2161b224
Omkar Mundada <83573933+OmkarMundada@users.noreply.github.com>
2021-05-24 10:36:34 +0530

    Update Subarrays.cpp

--- Subarrays.cpp
+++ Subarrays.cpp
@@ -143,3 +143,31 @@ int main()
     
     return 0;
 }
+
+
+//Maximum Subarray Sum
+
+
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+	    cin>>a[i];
+	}
+	int sum=0;
+	int maxsum=INT_MIN;
+	for(int i=0;i<n;i++){
+	    sum+=a[i];
+	    if(sum<0){
+	        sum=0;
+	    }
+	    maxsum=max(maxsum,sum);
+	}
+	cout<<maxsum<<endl;
+	return 0;
+}
