c5a85725ffc6dacb0234c0e0f1075790e70345b5
Omkar Mundada <83573933+OmkarMundada@users.noreply.github.com>
2021-12-12 13:33:11 +0530

    Update Array.cpp

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -162,3 +162,37 @@ int main() {
     cout<<res<<endl;
    return 0;
 }
+
+
+// Pair Sum(Two Sum) in Sorted Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int n,k;
+    cin>>n>>k;
+    
+    int a[n];   
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+	int low=0;
+	int high=n-1;
+	while(low<high){
+	    if(a[low]+a[high]==k){
+	        cout<<low<<" "<<high<<endl;
+	        return 0;
+	    }
+	    else if(a[low]+a[high]>k){
+	        high--;
+	    }
+	    else{
+	        low++;
+	    }
+	}
+	cout<<"-1"<<endl;
+	return 0;
+}
+
