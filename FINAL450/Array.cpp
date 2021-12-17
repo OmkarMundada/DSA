3f8b485d619280371e1e7c3daf7842fe7e40a866
Omkar Mundada <83573933+OmkarMundada@users.noreply.github.com>
2021-12-17 18:35:56 +0530

    Update Array.cpp

--- FINAL450/Array.cpp
+++ FINAL450/Array.cpp
@@ -1,1 +1,47 @@
+//  Reverse the array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	int j=n-1,temp;
+	for(int i=0;i<j;i++){
+	   temp=arr[i];
+	   arr[i]=arr[j];
+	   arr[j]=temp;
+	   j--;
+	}
+	
+	for(int i=0;i<n;i++){
+	    cout<<arr[i]<<" ";
+	}
+    return 0;
+}
+
+// Find the maximum and minimum element in an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	int mx=INT_MIN,mn=INT_MAX;
+	for(int i=0;i<n;i++){
+	   mn=min(mn,arr[i]);
+	   mx=max(mx,arr[i]);
+	}
+	cout<<mx<<" "<<mn<<endl;
+    return 0;
+}
 
