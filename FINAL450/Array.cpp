61dbd315823f52a183e7ba83f34e2bbfc17fa7da
Omkar Mundada <83573933+OmkarMundada@users.noreply.github.com>
2021-12-20 12:04:48 +0530

    Update Array.cpp

--- FINAL450/Array.cpp
+++ FINAL450/Array.cpp
@@ -61,3 +61,43 @@ int main() {
 	cout<<arr[k-1]<<" "<<arr[n-k]<<endl;
 	return 0;
 }
+
+// Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int a[n];
+	int count0=0,count1=0,count2=0;
+	for(int i=0;i<n;i++){
+	    cin>>a[i];
+	    if(a[i] == 0){
+            count0++;}
+        else if(a[i] == 1){
+            count1++;}
+        else{
+            count2++;}
+	}
+	for(int i=0;i<n;i++){
+	    if(count0 != 0){
+	        a[i]=0;
+	        count0--;
+	    }
+	    else if(count1 !=0){
+	        a[i]=1;
+	        count1--;
+	    }
+	    else if(count2 !=0){
+	        a[i]=2;
+	        count2--;
+	    }
+	}
+	
+	for(int i=0;i<n;i++){
+	    cout<<a[i]<<" ";
+	}		
+	return 0;
+}
