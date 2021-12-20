bddb0c65bb48f85030cb01f4a5ecb756b9026ff9
Omkar Mundada <83573933+OmkarMundada@users.noreply.github.com>
2021-12-20 14:07:35 +0530

    Update Array.cpp

--- FINAL450/Array.cpp
+++ FINAL450/Array.cpp
@@ -101,3 +101,31 @@ int main() {
 	}		
 	return 0;
 }
+
+// Move all the negative elements to one side of the array 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+	    cin>>a[i];
+	}
+	int j=0;
+	for(int i=0;i<n;i++){
+	    if(a[i]<0){
+	        if(i != j){
+	            swap(a[i],a[j]);
+	        }
+	        j++;
+	    }
+	}
+	for(int i=0;i<n;i++){
+	    cout<<a[i]<<" ";
+	}
+	return 0;
+}
+
