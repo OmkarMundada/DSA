db74833ca35611b18b838782674a00466bd85a78
Omkar Mundada <83573933+OmkarMundada@users.noreply.github.com>
2021-12-20 11:50:56 +0530

    Update Array.cpp

--- FINAL450/Array.cpp
+++ FINAL450/Array.cpp
@@ -45,3 +45,19 @@ int main() {
     return 0;
 }
 
+// Find the "Kth" max and min element of an array 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,k;
+	cin>>n>>k;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	cout<<arr[k-1]<<" "<<arr[n-k]<<endl;
+	return 0;
+}
